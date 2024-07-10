e76d8ff0cc008cb0af79637168ae7ddd0d0cea92
Simão Pedro <simaaoweb@gmail.com>
2024-07-10 02:53:38 +0100

    added functionality to contact form

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ gitHubBtn.addEventListener('click', () => {
 // const linkedin = document.getElementsByClassName('fa-linkedin-in')
 
 // linkedin.addEventListener('click', () => {
-    
+
 // })
 
 
@@ -41,3 +41,72 @@ xmarkBtn.addEventListener('click', () => {
     xmarkBtn.style.display = 'none'
     bars.style.display = 'flex'
 })
+
+// form message
+
+// CHECK IF NAME IS VALID
+const userName = document.querySelector('#name');
+const submitBtn = document.querySelector('.submit-button')
+const errorMessageName = document.querySelector('.errorMessageName')
+const invalidName = "Your name is invalid"
+
+userName.addEventListener('input', () => {
+
+    let regex = new RegExp (/^[a-z][a-z\s\ã\à\á\í\ó]+$/, "gi");
+    let string = userName.value;
+
+    if (!regex.test(string)) {
+        errorMessageName.textContent = invalidName
+    }
+
+    else {
+        errorMessageName.textContent = ''
+    }
+     checkAll()
+})
+
+// CHECK IF EMAIL IS VALID
+const email = document.querySelector('#email')
+const invalidEmail = "Your email is invalid"
+const errorMessageEmail = document.querySelector('.errorMessageEmail')
+
+
+email.addEventListener('input', () => {
+
+    let regex = /^([0-9]|[a-z]|.|_)+\b@\b[a-z]{2,6}\.[a-z]{2,3}$/g
+    let string = email.value;
+
+    if (!regex.test(string)) {
+        errorMessageEmail.textContent = invalidEmail
+    }
+
+    else {
+        errorMessageEmail.textContent = ''
+    }
+    checkAll()
+})
+
+function checkAll () {
+    let inputValues = document.querySelectorAll('.contactForm input')
+    inputValues.forEach(input => {
+        input.addEventListener('input', () => {
+            if (errorMessageName.textContent == invalidName || errorMessageEmail.textContent == invalidEmail) {
+                submitBtn.disabled = true
+            }
+            else {
+                submitBtn.disabled = false
+            }
+        })
+    })
+   
+}
+
+//subject message when received
+
+const newMessageSubject = document.querySelector('#newMessage')
+
+function newMessage () {
+    newMessageSubject.value = 'New message from ' + userName.value
+    console.log(newMessageSubject.value);
+}
+
